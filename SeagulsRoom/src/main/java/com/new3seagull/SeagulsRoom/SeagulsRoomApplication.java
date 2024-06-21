345619bdc06aa6259a77f75fc8b5174ad1ebd551
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-06-21 14:06:39 +0900

    init project

---
+++ SeagulsRoom/src/main/java/com/new3seagull/SeagulsRoom/SeagulsRoomApplication.java
@@ -0,0 +1,13 @@
+package com.new3seagull.SeagulsRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeagulsRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeagulsRoomApplication.class, args);
+	}
+
+}
---
+++ SeagulsRoom/src/test/java/com/new3seagull/SeagulsRoom/SeagulsRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.new3seagull.SeagulsRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeagulsRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
