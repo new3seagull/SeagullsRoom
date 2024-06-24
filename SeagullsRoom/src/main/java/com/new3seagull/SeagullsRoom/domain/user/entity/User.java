6a41b6dbcd5ef8d51b40ed38410866ab1244972b
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-06-25 02:28:20 +0900

    fix(User.java): pk 전략 설정

--- SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/user/entity/User.java
+++ SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/user/entity/User.java
@@ -2,8 +2,11 @@ package com.new3seagull.SeagullsRoom.domain.user.entity;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,10 +16,18 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String email;
     private String name;
     private String password;
     private String role;
+
+    @Builder
+    private User(String email, String name, String password, String role) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
 }
