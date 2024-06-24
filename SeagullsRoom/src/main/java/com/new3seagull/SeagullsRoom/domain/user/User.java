75e2a2422aa9bd49c54b330a05aed55db0244e19
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-06-24 21:33:51 +0900

    feat(User.java): user entity 생성

---
+++ SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/user/User.java
@@ -0,0 +1,21 @@
+package com.new3seagull.SeagullsRoom.domain.user;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    private Long id;
+
+    private String email;
+    private String username;
+    private String role;
+}
