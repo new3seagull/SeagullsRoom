461fe40e1196f490ca69a9a315aedf85a28c208e
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-06-25 01:20:35 +0900

    feat: ExceptionCode 구현

---
+++ SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/global/error/ExceptionCode.java
@@ -0,0 +1,20 @@
+package com.new3seagull.SeagullsRoom.global.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum ExceptionCode {
+    // 사용자 관련 에러
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
+    USER_FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없습니다."),
+
+
+    // 잘못된 접근
+    BAD_APPROACH(HttpStatus.BAD_REQUEST, "잘못된 접근입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
