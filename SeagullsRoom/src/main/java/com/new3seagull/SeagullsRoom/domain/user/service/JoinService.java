838995c3a76cd97241c833aad530ad7ca5d43506
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-06-25 22:14:18 +0900

    fix(JoinService): 이메일 중복 검사 예외 추가

--- SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/user/service/JoinService.java
+++ SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/user/service/JoinService.java
@@ -3,10 +3,13 @@ package com.new3seagull.SeagullsRoom.domain.user.service;
 import com.new3seagull.SeagullsRoom.domain.user.dto.JoinDto;
 import com.new3seagull.SeagullsRoom.domain.user.entity.User;
 import com.new3seagull.SeagullsRoom.domain.user.repository.UserRepository;
+import com.new3seagull.SeagullsRoom.global.error.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.new3seagull.SeagullsRoom.global.error.ExceptionCode.USER_EMAIL_ALREADY_EXISTS;
 
 @Service
 @RequiredArgsConstructor
@@ -21,16 +24,15 @@ public class JoinService {
 
         Boolean isExist = userRepository.existsByEmail(joinDTO.email());
 
-        if (isExist) {
-
-            return;
+        if (isExist) { // 이메일 중복 검사
+            throw new CustomException(USER_EMAIL_ALREADY_EXISTS);
         }
 
         User newUser = User.builder()
                 .name(joinDTO.name())
                 .email(joinDTO.email())
                 .password(bCryptPasswordEncoder.encode(joinDTO.password()))
-                .role("ROLE_ADMIN")
+                .role("ROLE_USER")
                 .build();
 
         userRepository.save(newUser);
