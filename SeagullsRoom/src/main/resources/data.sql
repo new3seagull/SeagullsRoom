c1802a36bb4193560f2a01661f51466376c0486a
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-06-25 22:18:13 +0900

    fix: user에서 user_tb로 테이블이름 수정

--- SeagullsRoom/src/main/resources/data.sql
+++ SeagullsRoom/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO User (email, name, password, role)
+INSERT INTO user_tb (email, name, password, role)
 VALUES ('abc1@example.com', 'name1', 'password123', 'ROLE_USER');
 
-INSERT INTO User (email, name, password, role)
+INSERT INTO user_tb (email, name, password, role)
 VALUES ('abc2@example.com', 'name2', 'password456', 'ROLE_USER');
