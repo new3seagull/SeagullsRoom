fb7b6c5bca226d6b51e2accaea2a3c2b2f954404
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-06-25 22:13:18 +0900

    feat(UserRepository): findUserByEmail 구현

--- SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/user/repository/UserRepository.java
+++ SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/user/repository/UserRepository.java
@@ -4,4 +4,8 @@ import com.new3seagull.SeagullsRoom.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Boolean existsByEmail(String email);
+
+    User findUserByEmail(String email);
 }
