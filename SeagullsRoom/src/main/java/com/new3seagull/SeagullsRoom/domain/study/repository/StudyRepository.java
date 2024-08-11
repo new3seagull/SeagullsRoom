e3ea0bbdbb7dea930bf30264fc1d061d2787c392
junhyeong Park <64734115+JunHyeong-99@users.noreply.github.com>
2024-08-11 23:24:09 +0900

    Revert "Feature/19 mypage"

--- SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/study/repository/StudyRepository.java
+++ SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/study/repository/StudyRepository.java
@@ -4,14 +4,8 @@ import com.new3seagull.SeagullsRoom.domain.study.entity.Study;
 import com.new3seagull.SeagullsRoom.domain.user.entity.User;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Tuple;
-import jakarta.persistence.TypedQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -21,28 +15,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Long> {
-
     List<Study> findAllByUser(User user);
 
+    Page<Study> findAllByStudyDateOrderByStudyTimeDesc(Pageable pageable, LocalDate studyDate);
     Study findByUserAndStudyDate(User user, LocalDate studyDate);
 
     @Query("SELECT s FROM Study s WHERE s.user = :user AND FUNCTION('YEAR', s.studyDate) = :year AND FUNCTION('MONTH', s.studyDate) = :month")
     List<Study> findByUserAndYearAndMonth(@Param("user") User user, @Param("year") int year, @Param("month") int month);
-
-    //사용자의 특정 날짜의 공부시간 조회 메서드
-    @Query("SELECT s.studyTime FROM Study s WHERE s.user = :user AND FUNCTION('YEAR', s.studyDate) = :year AND FUNCTION('MONTH', s.studyDate) = :month AND FUNCTION('DAY', s.studyDate) = :day")
-    List<LocalTime> findTotalStudyTimeByUserIdAndDate(@Param("user") User user, @Param("year") int year, @Param("month") int month, @Param("day") int day);
-
-    @Query("SELECT s.user, " +
-            "SUM(HOUR(s.studyTime) * 60 + MINUTE(s.studyTime)) AS totalStudyTimeInMinutes " +
-            "FROM Study s " +
-            "WHERE FUNCTION('YEAR', s.studyDate) = :year " +
-            "AND FUNCTION('MONTH', s.studyDate) = :month " +
-            "AND FUNCTION('DAY', s.studyDate) = :day " +
-            "GROUP BY s.user " +
-            "ORDER BY totalStudyTimeInMinutes DESC")
-    Page<Object[]> findStudyTimeRankingByDate(@Param("year") int year,
-                                              @Param("month") int month,
-                                              @Param("day") int day,
-                                              Pageable pageable);
 }
--- SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/study/service/StudyService.java
+++ SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/study/service/StudyService.java
@@ -11,7 +11,6 @@ import com.new3seagull.SeagullsRoom.domain.user.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import java.security.Principal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -61,28 +60,11 @@ public class StudyService {
 
     public List<StudyResponseDto> getTop10StudyTimes(LocalDate date) {
         Pageable pageable = PageRequest.of(0, 10);
-        List<Object[]> top10 = studyRepository.findStudyTimeRankingByDate(
-                date.getYear(), date.getMonthValue(), date.getDayOfMonth(), pageable).getContent();
-
-        return top10.stream()
-                .map(row -> {
-                    User user = (User) row[0];
-                    int totalStudyTimeInMinutes = ((Long) row[1]).intValue(); // 총 공부 시간을 분 단위로 받아옴
-
-                    // 분 단위 시간을 LocalTime으로 변환
-                    LocalTime studyTime = convertSecondsToLocalTime(totalStudyTimeInMinutes);
-
-                    // 현재 시간을 updatedAt으로 설정
-                    LocalDateTime updatedAt = LocalDateTime.now();
-
-                    // StudyResponseDto 객체 생성 및 반환
-                    return StudyResponseDto.builder()
-                            .userEmail(user.getEmail())
-                            .studyTime(studyTime)
-                            .updatedAt(updatedAt)
-                            .build();
-                })
-                .collect(Collectors.toList());
+        List<Study> top10Studies = studyRepository.findAllByStudyDateOrderByStudyTimeDesc(pageable,
+            date).getContent();
+        return top10Studies.stream()
+            .map(StudyResponseDto::toDto)
+            .collect(Collectors.toList());
     }
 
     // 유저의 특정 날짜의 공부 시간 조회
@@ -91,22 +73,8 @@ public class StudyService {
         if (user == null) {
             throw new CustomException(USER_NOT_FOUND);
         }
-
-        // StudyRepository에서 해당 날짜의 모든 공부 시간을 가져옴
-        List<LocalTime> totalStudyTimeByUserIdAndDate = studyRepository.findTotalStudyTimeByUserIdAndDate(
-                user, date.getYear(), date.getMonthValue(), date.getDayOfMonth());
-
-        // 모든 LocalTime의 분을 합산
-        int totalSeconds = totalStudyTimeByUserIdAndDate.stream()
-                .mapToInt(time -> time.getHour() * 3600 + time.getMinute() * 60 + time.getSecond())
-                .sum();
-
-
-        // LocalDate와 LocalTime을 결합하여 LocalDateTime 생성
-        return StudyResponseDto.builder()
-                .studyTime(convertSecondsToLocalTime(totalSeconds))
-                .userEmail(user.getEmail())
-                .build();
+        Study study = studyRepository.findByUserAndStudyDate(user, date);
+        return StudyResponseDto.toDto(study);
     }
 
     public List<StudyResponseDto> getStudyTimeByMonth(Principal principal, LocalDate date) {
@@ -120,13 +88,5 @@ public class StudyService {
         return study.stream()
             .map(StudyResponseDto::toDto)
             .collect(Collectors.toList());
-    }
-
-    private LocalTime convertSecondsToLocalTime(int totalSeconds) {
-        int hours = totalSeconds / 3600;
-        int minutes = (totalSeconds % 3600) / 60;
-        int seconds = totalSeconds % 60;
-
-        return LocalTime.of(hours, minutes, seconds);
     }
 }
