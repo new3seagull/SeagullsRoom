1dc108439862f37ce7005d44ac3cd04a714d9ae4
J-1ac <tkdwns26@naver.com>
2024-09-28 22:38:04 +0900

    feat: 스크린 타임 조회 시 오늘 기록만 조회되도록 수정

--- SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/screenTime/repository/ScreenTimeRepository.java
+++ SeagullsRoom/src/main/java/com/new3seagull/SeagullsRoom/domain/screenTime/repository/ScreenTimeRepository.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ScreenTimeRepository extends JpaRepository<ScreenTime, Long> {
 
-    @Query("SELECT st.category, SUM(st.count) FROM ScreenTime st WHERE st.user = :user GROUP BY st.category")
+    @Query("SELECT st.category, SUM(st.count) FROM ScreenTime st " +
+        "WHERE st.user = :user AND DATE(st.updatedAt) = CURRENT_DATE " +
+        "GROUP BY st.category")
     List<Object[]> findScreenTimesByUserGroupByCategory(@Param("user") User user);
 }
